e55052507a0fa4a5a4fc3f7820691dafa89c5945
김동환 <gidskql6671@gmail.com>
2021-02-18 10:34:46 +0000

    반응형 프로필 화면 작성중

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ const App = () => {
 	
 	let [toggleProfile, setToggleProfile] = useState(false);
 	
-	
 	return (
 		<div className="App">
 			<Container fluid className="container-navbar">
@@ -50,17 +49,16 @@ const App = () => {
 				</Col>
 			</Row>
 			
-			{
-				isNotPc && (
-					toggleProfile 
-					?
-					<div>
-						zz
-					</div>
-					:
-					<Image src="/images/profile.png" roundedCircle className="image-toggle-profile" onClick={
+			{	isNotPc && <Image src="/images/profile.png" roundedCircle className="image-toggle-profile" onClick={
 							() => {setToggleProfile(prev => !prev);}}/>
-					)
+			}
+			{
+				isNotPc && toggleProfile 
+				?
+				<div className="container-profile-mini arrow_box">
+					<Profile toggleImage={false}  />
+				</div>
+				: null
 			}
 		
 		</div>
@@ -68,11 +66,11 @@ const App = () => {
 	);
 };
 
-const Profile = () =>{
+const Profile = ({toggleImage = true, color = 'white'}) =>{
 	
 	return(
-		<div>
-			<Image src="/images/profile.png" roundedCircle className="image-profile mb-3" />
+		<div style={{color: color}}>
+			{ toggleImage && <Image src="/images/profile.png" roundedCircle className="image-profile mb-3" />}
 			<h4> 김동환 </h4>
 			<p> ASD AAA</p>
 		</div>
