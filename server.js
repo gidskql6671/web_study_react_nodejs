15aa3300c8e28f338cf6a01b3ae19e2d79ea8058
김동환 <gidskql6671@gmail.com>
2021-02-20 03:46:46 +0000

    gitignore update

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use( '/', express.static( path.join(__dirname, 'public') ));
+app.use( '/react', express.static( path.join(__dirname, 'client/build') ));
+
+
+app.get('/', (req, res) => {
+	res.sendFile( path.join(__dirname, 'index.html') );
+});
+app.get('/react/*', (req, res) => {
+  res.sendFile( path.join(__dirname, 'client/build/index.html') );
+});
+app.get('/pet', (req, res) => { 
+  res.send('펫용품')
+});
+app.post('/add', (req, res) => {
+	console.log(req.body);
+	res.send("전송 완료");
+});
+
+const http = require('http').createServer(app);
+http.listen(8080, function() {
+    console.log('listening on 8080')
+});
