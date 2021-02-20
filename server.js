dc0158bb47bec9668afe9cb6da713d71951d4cb8
김동환 <gidskql6671@gmail.com>
2021-02-20 11:56:57 +0000

    mongo db 연결

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const MongoClient = require('mongodb').MongoClient;
 const property = require('./properties.js');
 
 const dbLink = property.getDbLink();
+let db;
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use( '/', express.static( path.join(__dirname, 'public') ));
@@ -18,17 +21,31 @@ app.get('/', (req, res) => {
 app.get('/react/*', (req, res) => {
   res.sendFile( path.join(__dirname, 'client/build/index.html') );
 });
-app.get('/pet', (req, res) => { 
-  res.send('펫용품')
-});
 app.post('/add', (req, res) => {
 	console.log(req.body);
+	
+	db.collection('post').insertOne( {name: req.body.user_name, age: req.body.user_age }, () => {
+		console.log('저장 완료');
+	})
+	
 	res.send("전송 완료");
+});
+app.get('/list', (req, res)=> {
+	db.collection('post').find().toArray(function(err, data){
+		if (err) return console.log(err);
+		
+		console.log(data);
+		res.render('list.ejs', {posts: data});
+	})
+	
+	
 });
 
 
 MongoClient.connect(dbLink, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
 	if (err) return console.log(err);
+	db = client.db('test');
+	
 
 	const http = require('http').createServer(app);
 	http.listen(8080, function() {
