e79b04972c98eb6cdae94eb35aa2fd0d56dbbdd6
김동환 <gidskql6671@gmail.com>
2021-02-21 06:37:41 +0000

    mongodb 기능  및 jQuery 기능 적용 중

--- server.js
+++ server.js
@@ -22,13 +22,31 @@ app.get('/react/*', (req, res) => {
   res.sendFile( path.join(__dirname, 'client/build/index.html') );
 });
 app.post('/add', (req, res) => {
-	console.log(req.body);
+	let postId = db.collection('counter').findOne({name: 'postId'}, (err, data) => {
+		let postId = data.totalPost;
+		
+		db.collection('post').insertOne( { _id: (postId + 1), name: req.body.user_name, age: req.body.user_age }, () => {
+			
+			db.collection('counter').updateOne({name: 'postId'}, {$inc : {totalPost: 1}}, (err, data) => {
+				res.redirect('/');
+			});
+			
+		});
+	});
 	
-	db.collection('post').insertOne( {name: req.body.user_name, age: req.body.user_age }, () => {
-		console.log('저장 완료');
-	})
+});
+app.delete('/delete', (req, res) => {
+	let postId = parseInt(req.body._id);
 	
-	res.send("전송 완료");
+	db.collection('post').deleteOne({_id: postId});
+	res.status(200).send();
+});
+
+app.delete('/deleteAll', (req, res) => {
+	db.collection('post').deleteMany({});
+	db.collection('counter').updateOne({name: 'postId'}, {$set : {totalPost: 0}});
+	
+	res.send("삭제 완료");
 });
 app.get('/list', (req, res)=> {
 	db.collection('post').find().toArray(function(err, data){
@@ -45,7 +63,6 @@ app.get('/list', (req, res)=> {
 MongoClient.connect(dbLink, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
 	if (err) return console.log(err);
 	db = client.db('test');
-	
 
 	const http = require('http').createServer(app);
 	http.listen(8080, function() {
