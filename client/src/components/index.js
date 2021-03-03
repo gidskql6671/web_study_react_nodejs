8ba49aa96707999646edd9bafc7fc3178dd459e3
김동환 <gidskql6671@gmail.com>
2021-03-03 10:14:06 +0000

    디렉토리 구조 개편

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as App } from "./App";
+export { default as Home } from "./Home";
+export { default as About } from "./About";
+export { default as PageNotFound } from "./PageNotFound";
+export { default as Pagination } from "./Pagination";
+export { default as Post } from "./Post";
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+import { App } from 'components';
 import 'scss/index.scss';
 
 ReactDOM.render(
