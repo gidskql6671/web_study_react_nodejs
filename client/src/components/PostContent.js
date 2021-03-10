45d797d5c929a959acbf5efce5f2eca59f428786
김동환 <gidskql6671@gmail.com>
2021-03-10 10:35:18 +0000

    글 자세히 보기 컴포넌트

---
+++ client/src/components/PostContent.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import 'scss/PostContent.scss';
+
+
+const PostContent = () => {
+	
+	
+	return (
+		<div>
+			
+		</div>
+		
+	);
+};
+
+
+
+export default PostContent;
