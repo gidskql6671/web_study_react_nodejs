17aedfaed15278bae196a4039ce31ca226d198b1
김동환 <gidskql6671@gmail.com>
2021-03-17 11:43:00 +0000

    로그인 기능 구현 중
    Serialize & Deserialize User
    passport local strategy

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,13 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../lib/models/User');
 
+// Serialize & Deserialize User
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+})
+passport.deserializeUser((id, done) => {
+	User.findOne({})
+})
+
+// 추후 작업...
